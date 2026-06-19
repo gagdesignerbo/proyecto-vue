851da6df8f18592420d464cea475158855d75ef1
gagdeveloper <gablabs12@gmail.com>
2026-06-19 00:02:05 -0400

    cree un componente de contador

---
+++ src/components/contador.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>HOLA MUNDO!</h1>
+  <h2>
+    Contador: <span>{{ contador }}</span>
+  </h2>
+  <button @click="aumentar">Aumentar</button>
+  <button @click="disminuir">Disminuir</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const contador = ref(0)
+
+const aumentar = () => {
+  contador.value++
+}
+
+const disminuir = () => {
+  contador.value--
+}
+</script>
+
+<style scoped>
+span {
+  color: red;
+}
+</style>
