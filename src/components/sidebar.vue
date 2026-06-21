58a9407d3ca0d7adf357cfecf193f74041341992
gagdeveloper <gablabs12@gmail.com>
2026-06-21 00:39:13 -0400

    mejorando el repositorio

---
+++ src/components/sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="sidebar">
+    <h2>Barra Lateral</h2>
+    <ul>
+      <li><router-link to="/contador">Contador</router-link></li>
+      <li><router-link to="/lista-de-tareas">Lista de Tareas</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.sidebar {
+  width: 200px;
+  padding: 20px;
+  background-color: #ffffff;
+}
+</style>
